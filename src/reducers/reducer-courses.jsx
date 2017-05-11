f5932b67e5e9a18c665485bafebb4b0f860c646b
Aori Nevo <aori.nevo@bloomingdales.com>
2017-05-10 22:16:03 -0400

    Build dist into test environment. Add local bootstrap.

--- src/reducers/reducer-courses.jsx
+++ src/reducers/reducer-courses.jsx
@@ -5,7 +5,7 @@ export default function( state = [], action ){
   switch ( action.type ){
     case FETCH_COURSES:
       console.log(action);
-      return action.payload.course_list;
+      return action.payload.data.course_list;
       break;      
     default:
       return state;
