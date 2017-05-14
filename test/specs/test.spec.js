bde8622097448b0498d225581bbb7e488ae4bff6
Aori Nevo <aori.nevo@bloomingdales.com>
2017-05-14 10:00:42 -0400

    Setting up unit tests.

---
+++ test/specs/test.spec.js
@@ -0,0 +1,10 @@
+import expect from 'expect';
+
+describe('Initial setup', ()=>{
+  it('should pass in first test', () => {
+    expect(1).toBe(1);
+  });
+  // it('should fail in second test', () => {
+  //   expect(1).toBe(0);
+  // });
+});
